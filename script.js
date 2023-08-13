9e32a40023de7ef49899d935bcdda379b46fc8c7
Ashwini3131 <93239185+Ashwini3131@users.noreply.github.com>
2023-08-13 21:13:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
 function threeSum(arr, target) {
 // write your code here
-  arr.sort((a, b) => a - arrb);  // Sort the array in ascending order
-    let closestSum = arr[0] + arr[1] + arr[2];  // Initialize closestSum with the sum of the first three elements
+  arr.sort((a, b) => a - b);
+  
+  // Initialize variables
+  let closestSum = arr[0] + arr[1] + arr[2];
+  
+  // Iterate through the array
+  for (let i = 0; i < arr.length - 2; i++) {
+    let left = i + 1;
+    let right = arr.length - 1;
     
-    for (let i = 0; i < arr.length - 2; i++) {
-        let left = i + 1;
-        let right = arr.length - 1;
-        
-        while (left < right) {
-            const currentSum = arr[i] + arr[left] + arr[right];
-            
-            if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
-                closestSum = currentSum;
-            }
-            
-            if (currentSum < target) {
-                left++;
-            } else {
-                right--;
-            }
-        }
+    while (left < right) {
+      const sum = arr[i] + arr[left] + arr[right];
+      
+      // Check if the current sum is closer to the target
+      if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+        closestSum = sum;
+      }
+      
+      if (sum < target) {
+        left++;
+      } else if (sum > target) {
+        right--;
+      } else {
+        // Found an exact match, so return the sum
+        return sum;
+      }
     }
-    
-    return closestSum;
-
+  }
+  
+  return closestSum;
+  
 }
 
 module.exports = threeSum;
